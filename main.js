345d1ed49d9aee0ce0d6af14c143de5f687a5b02
Bruno Xavier <bx9407@gmail.com>
2022-08-19 00:25:47 -0300

    Portifolio

---
+++ main.js
@@ -0,0 +1,15 @@
+//Type writer
+
+function typeWriter(elemento){
+    const textoArray = elemento.innerHTML.split('')
+    elemento.innerHTML = ''
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => {
+            elemento.innerHTML += letra
+        }, 150 * i)
+    });
+}
+const titulo = document.querySelector('.nome');
+typeWriter(titulo) 
+
+
