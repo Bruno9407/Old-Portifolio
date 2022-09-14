49ed19993f956c760a0d12f342e2f42181f53494
Bruno Xavier <bx9407@gmail.com>
2022-09-14 14:40:13 -0300

    animações

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-//Type writer
+const titulo = document.querySelector('.nome')
+const foto = document.querySelector(".eu")
+const projetos = document.querySelector(".projetos")
+const contato = document.querySelector(".contato")
+const targets = [projetos, contato]
+const animationClass = "anime"
 
 function typeWriter(elemento){
     const texto = "Bruno Xavier"
@@ -11,11 +16,6 @@ function typeWriter(elemento){
     });
 }
 
-const titulo = document.querySelector('.nome');
-
-
-
-const foto = document.querySelector(".eu")
 setTimeout(()=>{
     typeWriter(titulo)
     if(window.innerWidth >= 1000){
@@ -25,3 +25,21 @@ setTimeout(()=>{
 }, 1000)
 
 
+function animeScroll(){
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4)
+    targets.forEach((elemento)=>{
+        if((windowTop) > elemento.offsetTop){
+            elemento.classList.add(animationClass)
+        } else{
+            elemento.classList.remove(animationClass)
+        }
+    })
+}
+
+animeScroll();
+
+if(targets.length){
+    window.addEventListener('scroll', ()=>{
+        animeScroll();
+    })
+}
