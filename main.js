205d6f0480c461534c6b5daccfc6fadc0a8e16ee
Bruno Xavier <bx9407@gmail.com>
2022-11-29 16:15:58 -0300

    techs

--- main.js
+++ main.js
@@ -6,6 +6,23 @@ const animationClass = "anime"
 
 const menuItens = document.querySelectorAll('.menu-top a[href^="#"]')
 
+const techs = document.querySelectorAll(".tech")
+const descArea = document.querySelector(".descricao-tech")
+
+const descricoes = {
+    html: "HTML é uma linguagem de marcação de texto, estudo essa tecnologia a 3 anos e é a base para criação de paginas web.",
+
+    css: "CSS é uma linguagem de estilização que estudo a 3 anos, no inico tinha bastante dificuldade para fazer oque eu queria, porém depois que aprendi o flexbox e o grid foi só alegria.",
+
+    js: "JavaScript é uma linguagem de programação do navegador, já estudo essa linguagem a 2 anos e atualmente é a que mais estudo por causa do seu potencial de ser usada tando no front-end como no back-end com NodeJS.",
+
+    react: "React é um framework para criação de interface de usuário, estudo essa tecnologia à um pouco mais de 1 ano.",
+
+    node: "Node é um interpretador de Javascript para que possa ser usado fora do navegador, estudo Node por a menos de 1 ano, estudo para usar no back-end de aplicações web junto com Express e MySQL.",
+
+    python: "Python foi a primeira linguagem de programação que aprendi quando tinha 13 anos, desde então aprendi a fazer automação de processos e analize de dados, ainda estudo sobre ciência de dados e Machine Learning."
+}
+
 /*type writer e animação de entrada */
 
 function typeWriter(elemento){
@@ -40,7 +57,7 @@ function animeScroll(){
     })
 }
 
-animeScroll();
+
 
 if(targets.length){
     window.addEventListener('scroll', ()=>{
@@ -61,4 +78,16 @@ menuItens.forEach((item) => {
             behavior: "smooth"
         })
     })
+})
+
+/*Mostra descrição tecnologias*/
+
+function mostraDescricao(event){
+    const element = event.currentTarget
+    const tech = element.getAttribute("id")
+    descArea.innerHTML = descricoes[tech]
+}
+
+techs.forEach((item) =>{ 
+    item.addEventListener("click", mostraDescricao)
 })
